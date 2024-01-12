977e2d5bfbf75e7635c697ba0e604820a5973d5e
kkonii <119216472+kkonii@users.noreply.github.com>
2024-01-12 21:58:33 +0900

    Feat(player) : `hp, mp` 초기값 합 검증 기능

--- src/main/java/bossmonster/domain/Player.java
+++ src/main/java/bossmonster/domain/Player.java
@@ -2,18 +2,20 @@ package bossmonster.domain;
 
 public class Player {
     private static final int MAXIMUM_NAME_LENGTH = 5;
+    private static final int INIT_POINTS_SUMMARY = 200;
 
     private final String name;
-    private int mp;
     private int hp;
+    private int mp;
 
     //for user
-    public Player(String name, int mp, int hp) {
+    public Player(String name, int hp, int mp) {
         validateNameLength(name);
         validateBlank(name);
+        validatePointsSum(hp, mp);
         this.name = name;
-        this.mp = mp;
         this.hp = hp;
+        this.mp = mp;
     }
 
     private void validateNameLength(String name) {
@@ -25,6 +27,12 @@ public class Player {
     private void validateBlank(String name) {
         if (name.isBlank() || name.contains(" ")) {
             throw new IllegalArgumentException("[ERROR] 공백은 이름에 입력할 수 없습니다.");
+        }
+    }
+
+    private void validatePointsSum(int hp, int mp) {
+        if (hp + mp != INIT_POINTS_SUMMARY) {
+            throw new IllegalArgumentException("[ERROR] 플레이어의 초기 hp와 mp의 합은 " + INIT_POINTS_SUMMARY + "가 되어야 합니다.");
         }
     }
 }

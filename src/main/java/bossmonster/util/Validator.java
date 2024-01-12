11c90802d69a810a50e8f128aa844ae2927dee5e
kkonii <119216472+kkonii@users.noreply.github.com>
2024-01-12 21:25:45 +0900

    Feat(validator) : 제시한 `구분자`로 구분한 여부 검증 기능

--- src/main/java/bossmonster/util/Validator.java
+++ src/main/java/bossmonster/util/Validator.java
@@ -2,10 +2,18 @@ package bossmonster.util;
 
 public class Validator {
     public static final String SEPARATOR = ",";
+    private static final String WRONG_POINT_FORMAT = "\\w+[^" + SEPARATOR + "]\\w+";
 
     public static String validatePointFormat(String points) {
         if (points.startsWith(SEPARATOR) || points.endsWith(SEPARATOR)) {
             throw new IllegalArgumentException("[ERROR] " + SEPARATOR + "로 시작하거나 끝낼 수 없습니다.");
+        }
+        return points;
+    }
+
+    public static String validateRegex(String points) {
+        if (points.matches(WRONG_POINT_FORMAT)) {
+            throw new IllegalArgumentException("[ERROR] " + SEPARATOR + "로 구분하여 입력해 주세요.");
         }
         return points;
     }
